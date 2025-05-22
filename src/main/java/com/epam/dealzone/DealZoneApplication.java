9412f13fd9a27b00186589aac1db1bba19dda461
BelyavtsevRS <belyavtsev.r.s@gmail.com>
2025-05-22 20:16:13 +0500

    init project

---
+++ src/main/java/com/epam/dealzone/DealZoneApplication.java
@@ -0,0 +1,13 @@
+package com.epam.dealzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealZoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DealZoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/dealzone/DealZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.dealzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealZoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
